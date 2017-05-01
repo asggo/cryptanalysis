79498c0037b30a76c0716e6dc1085b7b79e950b2
averagesecurityguy <stephen@averagesecurityguy.info>
2017-04-30 23:36:59 -0400

    Initial commit of Xor package.

---
+++ xor/xor.go
@@ -0,0 +1,24 @@
+package xor
+
+import (
+    errors
+)
+
+
+func ByteArrays(b1, b2 []byte) []byte {
+    e := error
+
+    if len(b1) != len(b2) {
+        e = errors.New("Cannot join byte arrays of two different lengths.")
+        return nil, e
+    }
+
+
+    result := make([]byte, len(b1))
+
+    for i, _ := range b1 {
+        result[i] = b1[i] ^ b2[i]
+    }
+
+    return result, nil
+}
