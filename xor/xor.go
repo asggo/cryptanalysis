dbf70df850930cf7c6fdc944eb9ae2d9372d4628
averagesecurityguy <stephen@averagesecurityguy.info>
2017-04-30 23:44:39 -0400

    Fix return errors.

--- xor/xor.go
+++ xor/xor.go
@@ -5,16 +5,16 @@ import (
 )
 
 
-func ByteArrays(b1, b2 []byte) []byte {
-    e := error
+func ByteArrays(b1, b2 []byte) ([]byte, error) {
+    var e error
+    var result []byte
 
     if len(b1) != len(b2) {
         e = errors.New("Cannot join byte arrays of two different lengths.")
         return nil, e
     }
 
-
-    result := make([]byte, len(b1))
+    result = make([]byte, len(b1))
 
     for i, _ := range b1 {
         result[i] = b1[i] ^ b2[i]
