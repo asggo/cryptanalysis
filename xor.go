f48649781ecd96009542ae1565aef6102b49eb36
averagesecurityguy <stephen@averagesecurityguy.info>
2017-05-01 03:28:16 -0400

    Separate Xor functions.

--- xor.go
+++ xor.go
@@ -4,23 +4,31 @@ import (
 )
 
 
-func XOR(b1, b2 []byte) []byte {
+func XorBytes(b1, b2 byte) byte {
+    return b1 ^ b2
+}
+
+
+func XorArrayByte(b1 []byte, b2 byte) []byte {
     result := make([]byte, len(b1))
 
-    if len(b2) == 1 {
-        // b2 is a single byte. XOR each byte of b1 with the byte in b2.
-        for i, _ := range b1 {
-            result[i] = b1[i] ^ b2[0]
-        }
-    } else {
-        // b2 is not a single byte. XOR each byte of b1 with each byte of b2.
-        if len(b1) != len(b2) {
-            panic("Cannot join byte arrays of two different lengths.")
-        }
+    for i, _ := range b1 {
+        result[i] = b1[i] ^ b2
+    }
 
-        for i, _ := range b1 {
-            result[i] = b1[i] ^ b2[i]
-        }
+    return result
+}
+
+
+func XorArrays(b1, b2 []byte) []byte {
+    result := make([]byte, len(b1))
+
+    if len(b1) != len(b2) {
+        panic("Cannot join byte arrays of two different lengths.")
+    }
+
+    for i, _ := range b1 {
+        result[i] = b1[i] ^ b2[i]
     }
 
     return result
