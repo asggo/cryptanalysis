dfc6a41822dbcdb78fde901372c9a1604542e661
averagesecurityguy <stephen@averagesecurityguy.info>
2017-04-30 23:19:10 -0400

    Initial commit of encoding package.

---
+++ encoding.go
@@ -0,0 +1,27 @@
+package encoding
+
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
+
+
+func DecodeB64Str(data string) ([]byte, error) {
+    return base64.StdEncoding.DecodeString(data)
+}
+
+
+func EncodeB64Str(data []byte) string {
+    return base64.StdEncoding.EncodeToString(data)
+}
+
+
+func DecodeHexStr(data string) ([]byte, error) {
+    return hex.DecodeString(data)
+}
+
+
+func EncodeHexStr(data []byte) string {
+    return hex.EncodeToString(data)
+}
