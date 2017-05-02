c6f4576a0cde4282684de31bf85928eea350e711
averagesecurityguy <stephen@averagesecurityguy.info>
2017-05-02 00:03:08 -0400

    Pad all data not just block_size chunks.

--- transform.go
+++ transform.go
@@ -5,10 +5,7 @@ import ()
 
 func Chunk(data []byte, size int) [][]byte {
     var chunks [][]byte
-
-    if len(data) % size != 0 {
-        data = PadPkcs7(data, size)
-    }
+    data = PadPkcs7(data, size)
 
     for i:=0; i<len(data); i=i+size {
         chunks = append(chunks, data[i:i+size])
@@ -35,9 +32,9 @@ func Transpose(data [][]byte) [][]byte {
 }
 
 
-func PadPkcs7(data []byte, size int) []byte {
-    if len(data) < size {
-        pad := size - len(data)
+func PadPkcs7(data []byte, block_size int) []byte {
+    if (len(data) % block_size) != 0 {
+        pad := len(data) / block_size
         for i:=0; i<pad; i++ {
             data = append(data, byte(pad))
         }
