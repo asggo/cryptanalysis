e68850eb27fb5d374546bf96501ac1b8969875a6
averagesecurityguy <stephen@averagesecurityguy.info>
2017-05-09 21:25:51 -0400

    Add additional en/decryption methods and tests.

--- crypt.go
+++ crypt.go
@@ -1,6 +1,13 @@
 package cryptanalysis
 
-import ()
+import (
+    "errors"
+    "crypto/aes"
+)
+
+
+const block_size = 16
+
 
 func EncryptXor(plain, key []byte) []byte {
 	var cipher []byte
@@ -11,4 +18,100 @@ func EncryptXor(plain, key []byte) []byte {
 	}
 
 	return cipher
+}
+
+
+func EncryptEcb(plaintext, key []byte) ([]byte, error) {
+    ciphertext := make([]byte, 0)
+    plaintext = PadPkcs7(plaintext, block_size)
+    chunks := Chunk(plaintext, block_size)
+
+    ecb, err := aes.NewCipher(key)
+    if err != nil {
+        return ciphertext, err
+    }
+
+    for _, chunk := range chunks {
+        temp := make([]byte, block_size)
+
+        ecb.Encrypt(temp, chunk)
+
+        ciphertext = append(ciphertext, temp...)
+    }
+
+    return ciphertext, nil
+}
+
+
+func DecryptEcb(ciphertext, key []byte) ([]byte, error) {
+    plaintext := make([]byte, 0)
+
+    if len(ciphertext) % block_size != 0 {
+        return plaintext, errors.New("Ciphertext is not padded properly.")
+    }
+
+    chunks := Chunk(ciphertext, block_size)
+    ecb, err := aes.NewCipher(key)
+    if err != nil {
+        return plaintext, err
+    }
+
+    for _, chunk := range chunks {
+        temp := make([]byte, block_size)
+
+        ecb.Decrypt(temp, chunk)
+
+        plaintext = append(plaintext, temp...)
+    }
+
+    return plaintext, nil
+}
+
+
+func EncryptCbc(plaintext, key []byte) ([]byte, error) {
+    iv := make([]byte, block_size)
+    ciphertext := make([]byte, 0)
+    chunks := Chunk(plaintext, block_size)
+
+    cbc, err := aes.NewCipher(key)
+    if err != nil {
+        return ciphertext, err
+    }
+
+    for _, chunk := range chunks {
+        chunk = PadPkcs7(chunk, block_size)
+        temp := make([]byte, block_size)
+
+        chunk = XorArrays(chunk, iv)
+        iv = chunk
+        cbc.Encrypt(temp, chunk)
+
+        ciphertext = append(ciphertext, temp...)
+    }
+
+    return ciphertext, nil
+}
+
+
+func DecryptCbc(ciphertext, key []byte) ([]byte, error) {
+    iv := make([]byte, block_size)
+    plaintext := make([]byte, 0)
+    chunks := Chunk(plaintext, block_size)
+
+    cbc, err := aes.NewCipher(key)
+    if err != nil {
+        return plaintext, err
+    }
+
+    for _, chunk := range chunks {
+        temp := make([]byte, block_size)
+
+        cbc.Decrypt(temp, chunk)
+        chunk = XorArrays(temp, iv)
+        iv = chunk
+
+        plaintext = append(plaintext, temp...)
+    }
+
+    return plaintext, nil
 }
